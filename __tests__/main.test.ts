b16e408fc3a8e8c12034e04c68e605df9f53d781
Austen Stone <austenstone@github.com>
2022-03-09 20:45:35 -0500

    try search

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,9 +2,10 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import { test } from '@jest/globals'
+import * as core from '@actions/core'
 
 test('test run', () => {
-  process.env['INPUT_GITHUB-TOKEN'] = process.env.GITHUB_TOKEN
+  process.env['INPUT_GITHUB-TOKEN'] = process.env.GITHUB_TOKEN || core.getInput('github-token')
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
