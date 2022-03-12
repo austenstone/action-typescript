fe406716f971619717efcd1060e4e26b86371e72
Austen Stone <austenstone@github.com>
2022-03-12 16:40:40 -0500

    updates

--- dist/index.js
+++ dist/index.js
@@ -8321,7 +8321,11 @@ const run_1 = __importDefault(__nccwpck_require__(7764));
 
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -8360,7 +8364,7 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
       login
     }
   }`);
-    core.info(`Hello, ${login}`);
+    core.info(`Hello, ${login}!`);
 });
 exports["default"] = run;
 
