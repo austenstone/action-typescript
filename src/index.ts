e4dd0fad11bfb5a0c54f7b02083cb19c2e8c08e5
Austen Stone <austenstone@github.com>
2024-11-26 10:40:59 -0500

    fix: Update GitHub Actions workflow permissions and restore issue retrieval logging in index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,14 @@ info(JSON.stringify(input, null, 2));
 info(JSON.stringify(context.repo, null, 2));
 
 const octokit = getOctokit(input.token);
-info(JSON.stringify(octokit, null, 2));
 
 try {
   await group("Issues", async () => {
-    // const { data: issues } = await octokit.rest.issues.list(context.repo);
+    const { data: issues } = await octokit.rest.issues.list(context.repo);
 
-    // for (const issue of issues) {
-    //   info(`#${issue.number}: ${issue.title}`);
-    // }
+    for (const issue of issues) {
+      info(`#${issue.number}: ${issue.title}`);
+    }
   });
 } catch (e) {
   if (e instanceof Error) {
