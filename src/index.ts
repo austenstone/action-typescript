cff979b93811854bfa8cde2b06aa6755cf132d9f
Austen Stone <austenstone@github.com>
2024-11-26 09:55:50 -0500

    chore: Remove outdated auto-update dependencies workflow and update token input in workflows

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { getInput, info } from "@actions/core";
 import { getOctokit } from "@actions/github";
 
 const input = {
-  token: getInput("token"),
+  token: getInput("github-token"),
 };
 
 const octokit = getOctokit(input.token);
