17ff41c73bbe89f1afd71b0dc87819d03b6e23eb
Austen Stone <austenstone@github.com>
2024-11-26 10:21:21 -0500

    feat: Group issue logging for better organization in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getInput, info, error } from "@actions/core";
+import { getInput, info, error, group } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 
 const input = {
@@ -10,11 +10,13 @@ info(JSON.stringify(input, null, 2));
 const octokit = getOctokit(input.token);
 
 try {
-  const { data: issues } = await octokit.rest.issues.list(context.repo);
+  group("Issues", async () => {
+    const { data: issues } = await octokit.rest.issues.list(context.repo);
 
-  for (const issue of issues) {
-    info(`#${issue.number}: ${issue.title}`);
-  }
+    for (const issue of issues) {
+      info(`#${issue.number}: ${issue.title}`);
+    }
+  });
 } catch (e) {
   if (e instanceof Error) {
     error(e.message);
